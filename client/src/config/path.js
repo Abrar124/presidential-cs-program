4073d2339211367b6571241fb8563a3d9fe793b8
zeeshanhanif <zeeshanhanif@gmail.com>
2018-11-24 18:35:21 +0500

    updated config

---
+++ client/src/config/path.js
@@ -0,0 +1,11 @@
+export default class path {
+
+    static baseUrl = "";
+
+    static LOGIN = path.baseUrl + "/authenticate";
+
+    static APPLY = path.baseUrl + "/api/rest/apply/";
+
+    static TEST = path.baseUrl + "/api/rest/test";
+
+}
