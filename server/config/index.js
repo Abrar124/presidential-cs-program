f432615f68c5c9e456fb6ca92bff454f60cde3d3
zeeshanhanif <zeeshanhanif@gmail.com>
2018-11-24 20:04:47 +0500

    additional structure of server and integration of mongodb

---
+++ server/config/index.js
@@ -0,0 +1,10 @@
+exports = module.exports = function(app) {
+
+    // Use for development
+    app.set("mongodb-url",'"mongodb://localhost/mydb');
+
+    // Use for production
+    //app.set("mongodb-url","mongodb://user:pass@id.mongolab.com:port/database");
+    
+
+}
